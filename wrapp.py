ef2b255469487a53b872a9a418479919c8385f40
root <root@localhost.localdomain>
2020-03-27 16:11:28 -0600

    Comienzo actividad

--- wrapp.py
+++ wrapp.py
@@ -7,14 +7,6 @@ val2 = float (num2)
 def suma(val1=0, val2=0):
   return val1 + val2
 
-def resta(val1=0, val2=0):
-  return val1 - val2
-
-def mul(val1=0, val2=0):
-  return val1 * val2
-
-def div(val1=0, val2=0):
-  return val1 / val2
 
 def operacion(funcion, val1=0, val2=0):
   return funcion(val1, val2)
