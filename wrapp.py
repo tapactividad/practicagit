06f848e46b1c6b9b8433288e6e078f2af2a75143
root <root@localhost.localdomain>
2020-03-28 12:48:19 -0600

    Agregamos la resta

--- wrapp.py
+++ wrapp.py
@@ -7,6 +7,8 @@ val2 = float (num2)
 def suma(val1=0, val2=0):
   return val1 + val2
 
+def resta(val1=0, val2=0):
+  return val1-val2
 
 def operacion(funcion, val1=0, val2=0):
   return funcion(val1, val2)
