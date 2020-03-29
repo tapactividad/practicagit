fbe8c05ecf068d537ead182bfbaa5d54adb2ca6f
pedr0 <pedr0@localhost.localdomain>
2020-03-29 02:54:10 +0000

    agregamos la multiplicación

--- wrapp.py
+++ wrapp.py
@@ -9,6 +9,8 @@ def suma(val1=0, val2=0):
 
 def resta(val1=0, val2=0):
   return val1-val2
+def mul(val1=0, val2=0): 
+  return val1 * val2
 
 def operacion(funcion, val1=0, val2=0):
   return funcion(val1, val2)
