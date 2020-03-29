61bd7f66429423f7d3eb4324aac292d8f14db400
root <root@localhost.localdomain>
2020-03-29 04:34:40 +0000

    Agregamos la division

--- wrapp.py
+++ wrapp.py
@@ -12,6 +12,9 @@ def resta(val1=0, val2=0):
 def mul(val1=0, val2=0): 
   return val1 * val2
 
+def div(val1=0, val2=0):                
+  return val1 / val2
+
 def operacion(funcion, val1=0, val2=0):
   return funcion(val1, val2)
 
